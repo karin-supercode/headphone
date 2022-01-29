f9ce91dbe5632d104898c0b922aaef07dfb03742
Supercoder <supercoder@AirvonSpercoder.local>
2022-01-29 21:22:14 +0100

    Projekt Headphone Inspiration

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let firstName = document.getElementById("firstName")
+let lastName = document.getElementById("lastName")
+let email = document.getElementById("email")
+
+function werteAusForm() {
+  console.log(firstName.value);
+  console.log(lastName.value);
+  console.log(email.value);
+}
